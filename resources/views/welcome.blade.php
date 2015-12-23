b51257603681fefff70b934c35f8d3960515ea52
sureshkumarks1 <sureshkumarks1@gmail.com>
2015-12-23 15:37:37 +0530

    added by line

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,7 +40,7 @@
 	<body>
 		<div class="container">
 			<div class="content">
-				<div class="title">hi welcome</div>
+				<div class="title">hi welcome. By suresh kumar </div>
 				
 			</div>
 		</div>
